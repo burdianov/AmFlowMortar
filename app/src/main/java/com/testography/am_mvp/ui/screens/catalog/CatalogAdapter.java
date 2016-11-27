df155a1174a6bb26b9cda97746f9f04cab6fb694
Leo Burdianov <leo.burdianov@gmail.com>
2016-11-27 22:20:41 +0400

    2:15:07

--- app/src/main/java/com/testography/am_mvp/ui/screens/catalog/CatalogAdapter.java
+++ app/src/main/java/com/testography/am_mvp/ui/screens/catalog/CatalogAdapter.java
@@ -2,9 +2,11 @@ package com.testography.am_mvp.ui.screens.catalog;
 
 import android.content.Context;
 import android.support.v4.view.PagerAdapter;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.testography.am_mvp.R;
 import com.testography.am_mvp.data.storage.dto.ProductDto;
 
 import java.util.ArrayList;
@@ -38,8 +40,10 @@ public class CatalogAdapter extends PagerAdapter {
         ProductDto product = mProductList.get(position);
         // TODO: 27-Nov-16 create mortar context for product screen
         Context productContext = null;
-
-        return super.instantiateItem(container, position);
+        View newView = LayoutInflater.from(productContext).inflate(R.layout
+                .screen_product, container, false);
+        container.addView(newView);
+        return newView;
     }
 
     @Override
